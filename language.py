0932e4a236d11cf67d7c29919e34fc6c01e65347
Joanne <gitarijoanne@gmail.com>
2022-12-05 18:35:51 +0300

    first commit

---
+++ language.py
@@ -0,0 +1,11 @@
+#regular language detector 
+# from langdetect import detect
+
+# print(detect("this is language detection"))
+
+# print(detect("hii ni nini"))
+
+# checks confidence 
+from langdetect import detect_langs
+
+print(detect_langs("this is language detection "))
